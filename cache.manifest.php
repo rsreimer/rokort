1ad299d493ebe104428ce0479d57240e30e1299a
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-03-10 13:38:43 +0100

    Added web application.

---
+++ cache.manifest.php
@@ -0,0 +1,17 @@
+<?php 
+    header("Cache-Control: max-age=0, no-cache, no-store, must-revalidate");
+    header("Pragma: no-cache");
+    header("Expires: Wed, 11 Jan 1984 05:00:00 GMT");
+    header('Content-type: text/cache-manifest');
+?>CACHE MANIFEST
+# version 2.2.0
+
+app.css
+app.js
+logo.png
+//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css
+//ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js
+//ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-animate.min.js
+
+NETWORK:
+*
